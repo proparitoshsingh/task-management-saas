0a5c9ef724b59d9252b8f3f34d6dd8c709945dbd
Paritosh Singh <paritoshsingh1609@gmail.com>
2024-06-01 10:43:05 +0530

    feat: create new user route

---
+++ src/routes/users.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import { users } from '../db/Database';
+import { User } from '../models/User';
+
+const router = express.Router();
+
+router.post('/', (req: Request, res: Response) => {
+   const userId: string = uuidv4();
+   const newUser: User = { id: userId, tasks: [] };
+   users.set(userId, newUser);
+   res.status(201).send({ id: userId });
+});
+
+export default router;
